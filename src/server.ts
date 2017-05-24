c72a96031c55c34f4aa083834e56ff33a06075d2
Valentin Levshits <levshits.l@gmail.com>
2017-05-24 10:37:02 +0300

    Implemented starter boiletplate

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
-import * as Path from 'path';
-import * as Express from 'express';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import * as path from 'path';
 
-let app = Express();
-app.listen(3000);
+class Server {
+    public app: express.Application;
+
+    public static bootstrap(): Server {
+        return new Server();
+    }
+
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+
+    private config() {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use("/static", express.static(path.join(__dirname, "../static")));
+    }
+}
+var serverInstance = Server.bootstrap();
+serverInstance.app.listen(3000);
+
+
